67ecf9a7a1b26efb4ecd94e0e4dfda2710d64236
mambachristophersteven <christopherstevennunoo@gmail.com>
2023-07-14 18:26:02 -0900

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+function imageSlide1(){
+    document.querySelector("#pep").src = "./pepsi001.png"; 
+}
+function imageSlide2(){
+    document.querySelector("#pep").src = "./pepsi002.png"; 
+}
+function imageSlide3(){
+    document.querySelector("#pep").src = "./pepsi003.png"; 
+}
+
